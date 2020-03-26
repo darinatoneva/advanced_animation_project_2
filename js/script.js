1246aff86e317e93d74196a7a8714eb0399e2e6d
your actual name <your email address used to login at githubtonevadarina@gmail.com>
2020-03-26 07:31:49 +0100

    The modal works, needs styling

--- js/script.js
+++ js/script.js
@@ -52,20 +52,40 @@ function showSceneThree(data) {
 }
 
 //MODAL
-/*
-const modal = document.querySelector(".modal-background");
-modal.addEventListener("click", () => {
-  modal.classList.add("hide");
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+  }
+}
+//animation modal
+gsap.from("#myModal", {
+  duration: 5,
+  scale: 0,
+  ease: "linear"
 });
 
-function showDetails(data) {
-  console.log(data)
-  modal.querySelector(".modal-name").textContent = data.name;
-  modal.querySelector(".modal-description").textContent = data.longdescription;
-  modal.querySelector(".modal-price").textContent = data.price;
-  modal.classList.remove("hide");
-}
-*/
 
 //ANIMATIONS screen 2
 /*Animation lights*/
